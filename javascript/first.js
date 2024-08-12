41018b8af9ffb26987d3d026c84d6f4a02538a2d
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-12 11:24:53 +0530

    let and var

--- javascript/first.js
+++ javascript/first.js
@@ -6,3 +6,20 @@ function LocalVar() {
 }
 
 LocalVar();
+
+function H(){
+    console.log("Before Declaration", t);
+    var t =20;
+    console.log("After Declaration", t);
+}
+H();
+
+
+let globalVar = "i am global";
+console.log(globalVar);
+
+function localLet(){
+    let test = "test";
+    console.log(test);
+}
+localLet();
