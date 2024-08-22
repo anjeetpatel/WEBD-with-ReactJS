ec23eabf24183c76945e47535b3ba36e10d14375
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-22 16:17:43 +0530

    arrow

--- javascript/arrow.js
+++ javascript/arrow.js
@@ -1,17 +1,37 @@
 //arrow function for single expression
-const squre =x=>x*x;
+const squre = (x) => x * x;
 console.log(squre(4)); //16
 
 //arrow function with two parameters
 
-const add = (a,b)=> a+b;
-console.log(add(3,5));
+const add = (a, b) => a + b;
+console.log(add(3, 5));
 
 //arrow function with object literal
-const person = (name, age)=>({
-    name:name,
-    age:age,
+const person = (name, age) => ({
+  name: name,
+  age: age,
 });
 
-const personx = person('Anjeet',20);
+const personx = person("Anjeet", 20);
 console.log(personx); // { name: 'Anjeet', age: 20 }
+
+const createperson = (firstName, birthYear) => {
+  return {
+    firstName: firstName,
+    birthYear: birthYear,
+  };
+};
+
+const persone = createperson("ansk", 4454);
+console.log(persone);
+
+const obj4 ={
+    j:78,
+    h:function(){
+        const i =()=>{
+            console.log(e.log(this));
+            
+        }
+    }
+}
