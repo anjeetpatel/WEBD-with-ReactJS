5f0a7f0e94d4d5b9d8d3f06a6b38eea0c3ee4df4
anjeetpatel <anjeetpatel0306@gmail.com>
2024-09-05 15:45:30 +0530

    start

--- React/firstapp/src/App.js
+++ React/firstapp/src/App.js
@@ -1,8 +1,14 @@
 //jsx
 function JsxComponent(){
-  return(
+  return (
     //rule 1
-    <h1>Hello React </h1>
+    <>
+      <h1>Hello React </h1>
+      <p>this is paragraph</p>
+    </> //fragments
+    //rule 2 close every tag
+    //rule 3 is you have to do camelcasing
+
   );
 }
 export default JsxComponent;
