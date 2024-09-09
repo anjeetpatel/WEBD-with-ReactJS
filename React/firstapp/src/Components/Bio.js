b2ea40ebe30cda88c0ef8dc2feff8f09da0f88d7
anjeetpatel <anjeetpatel0306@gmail.com>
2024-09-09 10:29:53 +0530

    react props

--- React/firstapp/src/Components/Bio.js
+++ React/firstapp/src/Components/Bio.js
@@ -1,10 +1,10 @@
-const Bio = (props)=>{
-  return(
+const Bio = (props) => {
+  return (
     <div>
       <h1>My name is Anjeet</h1>
       <p>{props.bio}</p>
     </div>
   );
-}
+};
 
 export default Bio;
---
+++ React/firstapp/src/Components/Profile.js
@@ -0,0 +1,14 @@
+import Bio from './Bio';
+const Profile = ()=>{
+    const {name,age,bio}=props;
+    return (
+        <div>
+            <h1>Profile</h1>
+            <h2>Name:{name}</h2>
+            <h3>Age:{age}</h3>
+            {/* <p>Bio:{bio}</p> */}
+            <Bio bio={bio}/>
+            </div>
+    );
+}
+export default Profile;
