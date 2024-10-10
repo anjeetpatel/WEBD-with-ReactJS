4640696645f677fa5baf3f853d33b828778e945c
anjeetpatel <anjeetpatel0306@gmail.com>
2024-10-10 23:01:35 +0530

    new task

--- Activities/Activity 6/shopping-list-app/src/App.js
+++ Activities/Activity 6/shopping-list-app/src/App.js
@@ -3,13 +3,16 @@ import React from "react";
 import { ShoppingListProvider } from "./ShoppingListContext";
 import ItemInput from "./ItemInput";
 import ItemList from "./ItemList";
+import "./App.css"; // Import the CSS file
 
 const App = () => {
   return (
     <ShoppingListProvider>
-      <h1>Shopping List</h1>
-      <ItemInput />
-      <ItemList />
+      <div className="container">
+        <h1>Shopping List</h1>
+        <ItemInput />
+        <ItemList />
+      </div>
     </ShoppingListProvider>
   );
 };
